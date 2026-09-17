6142ba0c2392847967d994d5437a8242a8d1de68
divaaull <divaauliafitrii@gmail.com>
2026-09-17 19:43:29 +0700

    Menambahkan Pertemuan 4

---
+++ Pertemuan 4/tugas.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Tabel Perkalian</title>
+</head>
+<body>
+    <h3>Tabel Perkalian</h3>
+    <pre>
+==================
+<?php
+$faktor = 12;
+for ($i = 15; $i <= 45; $i += 2) {
+    $hasil = $faktor * $i;
+    echo "$faktor * $i = $hasil\n";
+}
+?>
+==================
+    </pre>
+</body>
+</html>
